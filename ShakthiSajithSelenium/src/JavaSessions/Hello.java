7befeb5468f667cd52e3c84859ddcaf9eb7ed626
lankeshleader <49361514+lankeshleader@users.noreply.github.com>
2019-10-07 11:12:35 +0530

    Initial Version

---
+++ ShakthiSajithSelenium/src/JavaSessions/Hello.java
@@ -0,0 +1,12 @@
+package JavaSessions;
+
+public class Hello {
+
+	public static void main(String[] args) 
+	{
+		//print the message that is given inside the double inverted quotes
+		System.out.println("Hello This is check");
+		
+	}
+
+}
