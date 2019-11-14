d938bc9a262ea4b220be2e6081a3f15b697bc0c5
lankeshleader <49361514+lankeshleader@users.noreply.github.com>
2019-11-14 13:37:58 +0530

    WebDriver

--- ShakthiSajithSelenium/src/SeleniumSession/webDriverMethods.java
+++ ShakthiSajithSelenium/src/SeleniumSession/webDriverMethods.java
@@ -3,8 +3,10 @@ package SeleniumSession;
 import java.util.List;
 import java.util.Set;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -52,6 +54,7 @@ public class webDriverMethods {
 		 
 		 driver.navigate().refresh();
 		 
+		 // set is a type of collection
 		 Set<Cookie> cook = driver.manage().getCookies();
 		
 		 System.out.println(cook);
@@ -59,6 +62,47 @@ public class webDriverMethods {
 		 //close and quit methods
 //		 driver.quit();
 		 
+		 
+		 //target locator methods...
+		 
+		
+		 
+		// driver.findElement(By.className("btn-danger")).click();
+		 
+		 //introduce that xyz external script 
+		 
+		 JavascriptExecutor js = (JavascriptExecutor)driver;
+		 
+		 js.executeScript("alert('Iam JS alert box');");
+		 
+		 //alert interface...
+		 Alert a = driver.switchTo().alert();
+		 
+		 System.out.println(a.getText());
+		 a.accept();
+		// a.dismiss();
+		
+		 
+		 
+		 driver.switchTo().activeElement();
+		 
+		 driver.get("http://demo.automationtesting.in/Frames.html");
+		 
+		 String currentwindow = driver.getWindowHandle();
+		 
+		 System.out.println(currentwindow);
+		 // switching to single frame in thw app
+		 driver.switchTo().frame("singleframe");
+		 
+		 // identifying the textbox inside the frame and then entering the value
+		 driver.findElement(By.xpath("//div[@class='col-xs-6 col-xs-offset-5']//input")).sendKeys("Hello");
+		
+		 driver.switchTo().window(currentwindow);
+		 driver.switchTo().parentFrame();
+		 
+		 driver.findElement(By.linkText("WebTable")).click();
+		
+		 driver.quit();
 		 
 		 
 	}
